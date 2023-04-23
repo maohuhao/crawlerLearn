4293e4e599781e71a7984d123d4ef269a66b7f5c
maohuahao <3242560235@qq.com>
2023-04-23 20:21:48 +0800

    add one cases

--- reverse_direction/7.cqie模拟登录.py
+++ reverse_direction/7.cqie模拟登录.py
@@ -1,14 +1,16 @@
 import requests
+import random
 
 if __name__ == '__main__':
-    url = 'http://a.cqie.edu.cn/cas/login?service=http%3A%2F%2Fi.cqie.edu.cn%2Fportal_main%2FtoPortalPage'
+    url = 'http://a.cqie.edu.cn/cas/login?service=http://i.cqie.edu.cn/portal_main/toPortalPage'
     session = requests.session()
     session.headers.update({
         'uesr-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
     })
     session.get(url=url)
+    JSESSIONID = session.cookies.get('JSESSIONID')
 
-    url2 = 'http://a.cqie.edu.cn/cas/login;jsessionid=' + session.cookies.get('JSESSIONID')
+    url2 = f"http://a.cqie.edu.cn/cas/login;jsessionid={session.cookies.get('JSESSIONID')}?service=http%3A%2F%2Fi.cqie.edu.cn%2Fportal_main%2FtoPortalPage"
 
     data = {
         "username": "209030111",
@@ -22,19 +24,28 @@ if __name__ == '__main__':
         "isMobileLogin": "false"
     }
 
-    resp = session.post(url2, data=data)
-    # print(session.cookies.get_dict())
+    session.post(url2, data=data)
 
-    # session.get(url='http://xs.cqie.edu.cn/xg/NoticeNewsList/viewListPage?type=1')
-    resp2 = session.get(url='http://xs.cqie.edu.cn/xg/ssoLogin.jsp?rdPath=backstageNoNav.jsp?loadMainPage=spring:allhdzyz/FromQXHDMHViewMH&ticket=ST-534824-OtaoJ6xlgYh4FXebP1TC-http%3A%2F%2Fa.cqie.edu.cn%2Fcas')
+    # 设置cookie为空
+    session.cookies.set('JSESSIONID', None)
+    #
+    # 更新cookie
+    c = requests.cookies.RequestsCookieJar()
+    c.set('JSESSIONID', JSESSIONID)
+    session.cookies.update(c)
     print(session.cookies.get_dict())
-    print(resp2.text)
 
-    # session.headers.update({'X-Requested-With': 'XMLHttpRequest'})
-    # session.headers.update({'Referer': 'http://xs.cqie.edu.cn/xg/backstageNoNav.jsp?loadMainPage=spring:allhdzyz/FromQXHDMHView'})
+    # 获取活动列表JSESSIONID
+    url3 = 'http://a.cqie.edu.cn/cas/login?service=http%3a%2f%2fxs.cqie.edu.cn%2fxg%2fssoLogin.jsp%3frdPath%3dbackstageNoNav.jsp%3floadMainPage%3dspring%3aallhdzyz%2fFromQXHDMHViewMH'
+    resp1 = session.get(url3, allow_redirects=False)
+    resp2 = requests.get(url=resp1.headers.get('Location'), headers={'uesr-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}, allow_redirects=False)
+    # 更新cookie
+    session.cookies.set('JSESSIONID', resp2.cookies.get('JSESSIONID'))
 
-    # url4 = 'http://xs.cqie.edu.cn/xg/allhdzyz/FromQXHDMHView?random=0.7484653543381663&random=0.10908594765094759'
-    # url4 = 'http://xs.cqie.edu.cn/xg/acp/system/login?_rd=100&rdPath=backstageNoNav.jsp?loadMainPage=spring:allhdzyz/FromQXHDMHViewMH'
-    # resp3 = session.post(url4)
-    # print(resp3.text)
+    # 将JSESSIONID保存在服务器
+    url4 = 'http://xs.cqie.edu.cn/xg/acp/system/login?_rd=100&rdPath=backstageNoNav.jsp?loadMainPage=spring:allhdzyz/FromQXHDMHViewMH'
+    session.post(url4)
 
+    # 获取活动
+    res = session.get(f'http://xs.cqie.edu.cn/xg/allhdzyz/FromQXHDMHViewMH?random={random.random()}&random={random.random()}')
+    print(res.text)
